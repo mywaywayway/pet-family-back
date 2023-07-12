444e874827157f9df0d17313206d95343d6aa4dc
Yeh-Wang <wyx1627082750@163.com>
2023-07-12 19:51:03 +0800

    YehWang

---
+++ src/main/java/com/pet/pro/entity/views/MerchantUserView.java
@@ -0,0 +1,79 @@
+package com.pet.pro.entity.views;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ *
+ * @TableName merchant_user_view
+ */
+@TableName(value ="merchant_user_view")
+@Data
+public class MerchantUserView implements Serializable {
+    /**
+     * 商家用户编号
+     */
+    @TableField(value = "merchant_user_id")
+    private Integer merchant_user_id;
+
+    /**
+     * 商家名称
+     */
+    @TableField(value = "merchant_name")
+    private String merchant_name;
+
+    /**
+     * 商家地址
+     */
+    @TableField(value = "merchant_address")
+    private String merchant_address;
+
+    /**
+     * 登录编号
+     */
+    @TableField(value = "login_id")
+    private Integer login_id;
+
+    /**
+     * 用户名
+     */
+    @TableField(value = "username")
+    private String username;
+
+    /**
+     * 密码
+     */
+    @TableField(value = "password")
+    private String password;
+
+    /**
+     * 性别
+     */
+    @TableField(value = "gender")
+    private String gender;
+
+    /**
+     * 头像
+     */
+    @TableField(value = "avatar")
+    private String avatar;
+
+    /**
+     * 电话
+     */
+    @TableField(value = "phone")
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    @TableField(value = "email")
+    private String email;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/pet/pro/service/impl/MerchantUserViewServiceImpl.java
@@ -0,0 +1,22 @@
+package com.pet.pro.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.pet.pro.entity.views.MerchantUserView;
+import com.pet.pro.service.MerchantUserViewService;
+import com.pet.pro.mapper.MerchantUserViewMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author YehWang
+* @description 针对表【merchant_user_view】的数据库操作Service实现
+* @createDate 2023-07-12 19:47:36
+*/
+@Service
+public class MerchantUserViewServiceImpl extends ServiceImpl<MerchantUserViewMapper, MerchantUserView>
+    implements MerchantUserViewService{
+
+}
+
+
+
+
