0af768ae3a2921b1019ca1b139348a49977e61ce
my way <827767408@qq.com>
2023-07-13 14:44:05 +0800

    my way

--- src/main/java/com/pet/pro/controller/StorageController.java
+++ src/main/java/com/pet/pro/controller/StorageController.java
@@ -6,11 +6,7 @@ import com.pet.pro.entity.StorageEntity;
 import com.pet.pro.service.impl.StorageServiceImpl;
 import lombok.experimental.Accessors;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -30,10 +26,37 @@ public class StorageController {
     public void setStorageServiceImpl(StorageServiceImpl storageServiceImpl) {
         this.storageServiceImpl = storageServiceImpl;
     }
+    /**
+     * 添加库存信息
+     * @param storageEntity 库存实体
+     * @return Result<?>
+     */
 
     @PostMapping("/addStorage")
     public Result<?>  addStorage(@RequestBody StorageEntity storageEntity){
         int flag=storageServiceImpl.addStorage(storageEntity);
+        if (flag==1){
+            return Result.success("添加成功");
+        }else {
+            return Result.fail("添加失败");
+        }
+    }
+    /**
+     * 查询所有库存信息
+     * @return Result<?>
+     */
+    @GetMapping("/selectAllStorage")
+    public Result<?> selectAllStorage(){
+        return Result.success(storageServiceImpl.selectAllStorage());
+    }
+    /**
+     * 添加库存数量
+     * @param storageEntity 库存实体
+     * @return Result<?>
+     */
+    @PostMapping("/addStorageQuantity")
+    public Result<?> addStorageQuantity(@RequestBody StorageEntity storageEntity){
+        int flag=storageServiceImpl.addStorageQuantity(storageEntity);
         if (flag==1){
             return Result.success("添加成功");
         }else {
--- src/main/java/com/pet/pro/service/impl/StorageServiceImpl.java
+++ src/main/java/com/pet/pro/service/impl/StorageServiceImpl.java
@@ -1,11 +1,14 @@
 package com.pet.pro.service.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.pet.pro.entity.StorageEntity;
 import com.pet.pro.mapper.StorageMapper;
 import com.pet.pro.service.StorageService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -19,6 +22,13 @@ import java.util.UUID;
 @Service
 public class StorageServiceImpl extends ServiceImpl<StorageMapper, StorageEntity> implements StorageService {
 
+    private StorageMapper storageMapper;
+    @Autowired
+    public void setStorageMapper(StorageMapper storageMapper) {
+        this.storageMapper = storageMapper;
+    }
+
+
     /**
      * 添加库存信息
       * @param storageEntity 库存实体
@@ -30,4 +40,25 @@ public class StorageServiceImpl extends ServiceImpl<StorageMapper, StorageEntity
         storageEntity.setStorageId(uuid.toString());
         return baseMapper.insert(storageEntity);
     }
+
+    /**
+     * 查询所有库存信息
+     * @return List<StorageEntity> 库存实体集合
+     * @see com.pet.pro.service.StorageService#selectAllStorage()
+     */
+    public List<StorageEntity> selectAllStorage() {
+        return baseMapper.selectList(null);
+    }
+    /**
+     * 添加库存数量
+     * @param storageEntity 库存实体
+     * @return 1?0 1：添加成功 0：添加失败
+     * @see com.pet.pro.service.StorageService#addStorageQuantity(StorageEntity)
+     */
+    public int addStorageQuantity(StorageEntity storageEntity) {
+        StorageEntity storageEntity1=storageMapper.selectOne(Wrappers.<StorageEntity>lambdaQuery().eq(StorageEntity::getCommodityId,storageEntity.getCommodityId()));
+        storageEntity1.setQuantity(storageEntity1.getQuantity()+storageEntity.getQuantity());
+        return storageMapper.updateById(storageEntity1);
+    }
+
 }
