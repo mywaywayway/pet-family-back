bb8b8af26e328f070b0e5f9becf170af366cdd48
Yeh-Wang <wyx1627082750@163.com>
2023-07-14 10:52:02 +0800

    添加登录后获取普通用户信息Controller

--- src/main/java/com/pet/pro/controller/RegularUserController.java
+++ src/main/java/com/pet/pro/controller/RegularUserController.java
@@ -1,15 +1,16 @@
 package com.pet.pro.controller;
 
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.pet.pro.Result;
 import com.pet.pro.entity.RegularUserEntity;
+import com.pet.pro.entity.views.RegularUserView;
 import com.pet.pro.mapper.RegularUserMapper;
+import com.pet.pro.mapper.RegularUserViewMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController("RegularUserController")
@@ -21,10 +22,25 @@ public class RegularUserController {
     public void setRegularUserMapper(RegularUserMapper regularUserMapper){
         this.regularUserMapper = regularUserMapper;
     }
+    RegularUserViewMapper regularUserViewMapper;
+    @Autowired
+    public void setRegularUserViewMapper(RegularUserViewMapper regularUserViewMapper){
+        this.regularUserViewMapper = regularUserViewMapper;
+    }
 
     @GetMapping("/getRegularUserById/{loginId}")
     public RegularUserEntity getRegularUserById(@PathVariable int loginId){
         return regularUserMapper.selectOne(new QueryWrapper<RegularUserEntity>().eq("login_id",loginId));
+    }
+
+    @PostMapping("/getUserInfo/{token}")
+    public Result<?> getInfo(@PathVariable String token){
+        DecodedJWT decode = JWT.decode(token);
+        String username = decode.getClaim("username").asString();
+        QueryWrapper<RegularUserView> wrapper = new QueryWrapper<>();
+        wrapper.eq("username",username);
+        RegularUserView regularUserView = regularUserViewMapper.selectOne(wrapper);
+        return Result.success(regularUserView,"个人信息");
     }
 
 }
