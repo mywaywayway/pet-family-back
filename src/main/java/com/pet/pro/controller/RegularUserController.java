90ed77303d06f347dfd9c0d08d12347a7038fd09
DoubleHong <greathongtian@gmail.com>
2023-07-14 11:27:56 +0800

    Hong

--- src/main/java/com/pet/pro/controller/RegularUserController.java
+++ src/main/java/com/pet/pro/controller/RegularUserController.java
@@ -2,14 +2,15 @@ package com.pet.pro.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.pet.pro.Result;
 import com.pet.pro.entity.RegularUserEntity;
+import com.pet.pro.entity.views.RegularUserView;
 import com.pet.pro.mapper.RegularUserMapper;
+import com.pet.pro.mapper.RegularUserViewMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 
 @RestController("RegularUserController")
@@ -22,10 +23,24 @@ public class RegularUserController {
         this.regularUserMapper = regularUserMapper;
     }
 
+    RegularUserViewMapper regularUserViewMapper;
+    @Autowired
+    public void setRegularUserViewMapper(RegularUserViewMapper regularUserViewMapper){
+        this.regularUserViewMapper = regularUserViewMapper;
+    }
+
     @GetMapping("/getRegularUserById/{loginId}")
     public RegularUserEntity getRegularUserById(@PathVariable int loginId){
         return regularUserMapper.selectOne(new QueryWrapper<RegularUserEntity>().eq("login_id",loginId));
     }
+
+    //查询所有用户
+    @GetMapping("/getAllRegularUser")
+    public Result<List<RegularUserView>> getAllRegularUser(){
+        List<RegularUserView> list = regularUserViewMapper.selectList(null);
+        return Result.success(list);
+    }
+
 
 }
 
